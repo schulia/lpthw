1eb7569c483ca30ebeaff61c8bddb7045d4ca1aa
Julia Moleiro <juliasmoleiro@gmail.com>
2020-06-11 13:56:42 -0300

    first files

---
+++ input.py
@@ -0,0 +1,16 @@
+#ex 11
+print("How old are you?", end=' > ')
+age = input()
+print("How tall are you?", end=' > ')
+height = input()
+print("How much do you weigh?", end=' > ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#ex 12
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
---
+++ read_and_write_files.py
@@ -0,0 +1,35 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file.  Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
+
+#to be called as ' python3 read_and_write_files.py myfile.txt '
